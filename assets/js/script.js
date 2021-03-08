eb49c241181173ac1d1ea3e4c333c62623cadcb8
MayurDhawad <35704330+MayurDhawad@users.noreply.github.com>
2021-03-08 11:59:52 +0530

    main

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+$('#owl-carousel1').owlCarousel({
+    loop:false,
+    margin:10,
+    nav:false,
+    dots: false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:2
+        }
+    }
+})
+
+$('#owl-carousel2').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
